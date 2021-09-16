05e4c5f3defd333aaafb23e26805830cbb5aaa89
Mustapha Abdelaziz <aziz.mus.19@gmail.com>
2021-09-16 10:18:00 +0100

    first commit

---
+++ src/dummy-data.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: '1',
+    fullName: 'Mustapha Abdelaziz',
+    projects: [
+      { id: 'max_p1', title: 'Record the Vue course' },
+      { id: 'max_p2', title: 'Create more courses' },
+      { id: 'max_p3', title: 'Keep content updated' }
+    ]
+  },
+  {
+    id: '2',
+    fullName: 'Hacene Chiekh Belhadj',
+    projects: [
+      { id: 'manu_p1', title: 'Create more courses' },
+      { id: 'manu_p2', title: 'Dive into data science topics' },
+      { id: 'manu_p3', title: 'Various things' }
+    ]
+  },
+  {
+    id: '3',
+    fullName: 'Ayoub Chebani',
+    projects: [
+      { id: 'julie_p1', title: 'Create agenda for next months' },
+      { id: 'julie_p2', title: 'Explore new topics' }
+    ]
+  },
+  {
+    id: '4',
+    fullName: 'Random Guy',
+    projects: []
+  }
+];
